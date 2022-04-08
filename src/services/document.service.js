3ea7729f6085a8b2c6b59618648f5ad7f1c2633b
Brayan-Perez-Martinez <pbryantsam@gmail.com>
2022-04-07 22:42:16 -0600

    added new logs

--- src/services/document.service.js
+++ src/services/document.service.js
@@ -16,30 +16,27 @@ export const sendDocuments = async (
   dateExpiry
 ) => {
   console.log(result)
-  console.log(photo)
-  console.log(dateBirth,typeof dateBirth)
-  console.log(dateIssue,typeof dateIssue)
-  console.log(dateExpiry, typeof dateExpiry)
+  console.log(dateBirth,typeof dateBirth,Number(dateBirth))
+  console.log(dateIssue,typeof dateIssue,Number(dateIssue))
+  console.log(dateExpiry, typeof dateExpiry,Number(dateExpiry))
   console.log(typeof result.documentNumber,Number(result.documentNumber))
 
-
-  const data = {
-    firstname: result.firstName,
-    lastname: result.lastName,
-    datebirth: dateBirth,
-    dateissue: dateIssue,
-    dateexpiry: dateExpiry,
-    numdocument: Number(result.documentNumber),
-    addres: result.address,
-    gender: result.sex,
-    marital_status: result.maritalStatus,
-    proffesion: result.profession,
-    photo: photo,
-    placebirth: result.placeOfBirth,
-  };
-  console.log(document);
-  apiClient.post("document", data).then(res => {
+  apiClient.post("document",{
+  firstname: result.firstName,
+  lastname: result.lastName,
+  datebirth: dateBirth,
+  dateissue: dateIssue,
+  dateexpiry: dateExpiry,
+  numdocument: Number(result.documentNumber),
+  addres: result.address,
+  gender: result.sex,
+  marital_status: result.maritalStatus,
+  proffesion: result.profession,
+  photo: photo,
+  placebirth: result.placeOfBirth
+}).then((res) => {
     console.log("res apiClient",res)
     return res.data;
   })
+
 };
